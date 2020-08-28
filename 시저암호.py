6fad56040c8cc05a8d28bce827db0990588ffdee
JJW-Jun <68919333+JJW-Jun@users.noreply.github.com>
2020-08-28 23:24:37 +0900

    Update 시저암호.py

--- 시저암호.py
+++ 시저암호.py
@@ -11,7 +11,13 @@ def solution(s, n) :
     answer = ''.join(s).strip()
     return answer
 
-print(solution('abcd', 3))
 
 
 
+# string은 immutable type이기 때문에 인덱스로 접근해서 수정할 수 없다.
+# 즉, a = 'abcd'에서 a[0] = 2라고 입력하면 에러가 나게 된다.
+
+
+# import string : 알파벳을 불러올 수 있다.
+# string.ascii_lowercase : 알파벳 소문자 
+# string.ascii_uppercase : 알파벳 대문자
