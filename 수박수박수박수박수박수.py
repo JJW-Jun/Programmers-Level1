579c779feb2e6dbef235487e73e6f465965b401d
JJW-Jun <68919333+JJW-Jun@users.noreply.github.com>
2020-09-01 23:07:26 +0900

    Update 수박수박수박수박수박수.py
    https://programmers.co.kr/learn/courses/30/lessons/12922?language=java

--- 수박수박수박수박수박수.py
+++ 수박수박수박수박수박수.py
@@ -1,4 +1,24 @@
+# Python Code
 def solution(n):
     word = "수박" * n
     answer = word[:n]
     return answer
+
+
+
+
+
+
+# Java Code
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        for (int i=1; i<=n; i++) {
+            if (i%2 !=0){
+                answer += "수";
+            } else {
+                answer += "박";
+            }
+        }return answer;
+    }
+}
