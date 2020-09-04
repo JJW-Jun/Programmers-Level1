7f563bd1fcbc340a23207ac4f5dd2ae4b07022ba
JJW-Jun <68919333+JJW-Jun@users.noreply.github.com>
2020-09-04 21:16:09 +0900

    Update 약수의 합.py
    https://programmers.co.kr/learn/courses/30/lessons/12928

--- 약수의 합.py
+++ 약수의 합.py
@@ -1,5 +1,8 @@
 def solution(n):
+    # 빈 리스트 생성합니다.
     answer = []
+    
+    # for문을 통해 n으로 1부터 n까지 나누어지는 수를 answer에 추가합니다.
     for i in range(1, n + 1):
         if n % i == 0:
             answer.append(i)
