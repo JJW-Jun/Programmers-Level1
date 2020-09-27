7fac30550d5ee221c8a5114311ae4615a4bc4474
JJW-Jun <68919333+JJW-Jun@users.noreply.github.com>
2020-09-28 04:45:30 +0900

    Update 수박수박수박수박수박수.py

--- 수박수박수박수박수박수.py
+++ 수박수박수박수박수박수.py
@@ -1,24 +1,5 @@
-# Python Code
+# 리스트에 *를 곱하면 " (문자 * 갯수) "가 되는 것을 활용.
 def solution(n):
     word = "수박" * n
     answer = word[:n]
     return answer
-
-
-
-
-
-
-# Java Code
-class Solution {
-    public String solution(int n) {
-        String answer = "";
-        for (int i=1; i<=n; i++) {
-            if (i%2 !=0){
-                answer += "수";
-            } else {
-                answer += "박";
-            }
-        }return answer;
-    }
-}
