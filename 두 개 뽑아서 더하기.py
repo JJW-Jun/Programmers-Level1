316071c7137a09a490a0e660204724226fabea11
Jun___ <68919333+JJW-Jun@users.noreply.github.com>
2020-09-28 13:40:35 +0900

    Update 두 개 뽑아서 더하기.py

--- 두 개 뽑아서 더하기.py
+++ 두 개 뽑아서 더하기.py
@@ -1,3 +1,4 @@
+# i < j 조건문을 통해 자기 자신 더하는 것을 방지.
 def solution(numbers) :
     answer = []
     for i in range(len(numbers)) :
@@ -9,6 +10,19 @@ def solution(numbers) :
 
 print(solution([5,0,2,7]))
 
-#
+
+
+
+# i와 j의 범위를 조절해서 자기 자신 더하는 것을 방지
+
+
+
+
+# 순열, 조합 활용
+
+
+
+
+
 # import random
 # print(list(set(random.choices(range(1000), k=20))))
