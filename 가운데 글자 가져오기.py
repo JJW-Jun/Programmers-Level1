0f58c6654de569f6f1697d6f8c1195b8cb632237
JJW-Jun <68919333+JJW-Jun@users.noreply.github.com>
2020-09-28 13:30:19 +0900

    Update 가운데 글자 가져오기.py

--- 가운데 글자 가져오기.py
+++ 가운데 글자 가져오기.py
@@ -1,9 +1,12 @@
+# 글자가 홀수일때는 단순히 가운데 글자만 가져오면 되지만 짝수일 경우 2글자가 오기 때문에 Case분류
 def solution(s) :
-    # 글자수가 홀수일때
+    
+    # 홀수
     if len(s) % 2 != 0 :
         answer = s[int((len(s)-1) /2)]
         return answer
-    # 글자수가 짝수일때
+    
+    # 짝수
     else :
         answer = s[int((len(s)/2 -1)):int((len(s)/2+1))]
         return answer
