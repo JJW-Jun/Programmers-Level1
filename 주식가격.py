e0b9347dca256f0378f155f8a7448c7d20ce4637
Jun___ <68919333+JJW-Jun@users.noreply.github.com>
2020-10-03 21:54:36 +0900

    Update 주식가격.py

--- 주식가격.py
+++ 주식가격.py
@@ -4,12 +4,16 @@ def solution(prices):
         second = 0
         for i in range(count + 1, len(prices)):
             if prices[count] > prices[i]:
-                second = i - count
+                second += 1
                 answer.append(second)
                 break
+
             else:
                 second += 1
-        if prices[count] <= prices[i]:
+        if prices[count] > prices[i]:
+            pass
+
+        else :
             answer.append(second)
         count += 1
 
